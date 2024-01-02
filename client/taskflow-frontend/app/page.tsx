a59a11a62d5c1c27fb870fb11a1c774e8dd1d410
Richard Emijere <emijere.richard@gmail.com>
2024-01-02 16:03:24 +0100

    minor changes in page.tsx

--- client/taskflow-frontend/app/page.tsx
+++ client/taskflow-frontend/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
+import Image from 'next/image';
 
 export default function Home() {
   return (

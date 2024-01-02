8157caab2ecf80300a8be828f75ce2a2fe0a0476
Richard Emijere <emijere.richard@gmail.com>
2024-01-02 15:59:40 +0100

    Frontend setup

---
+++ client/taskflow-frontend/app/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+export default function Home() {
+  return (
+    <main>
+      <Accordion type='single' collapsible>
+        <AccordionItem value='item-1'>
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </main>
+  );
+}

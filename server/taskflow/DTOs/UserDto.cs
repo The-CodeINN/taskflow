3e16b4ab092cdec602c1912254a95221f551c5c9
Richard Emijere <emijere.richard@gmail.com>
2024-01-03 14:20:09 +0100

    DTO: added image url

--- server/taskflow/DTOs/UserDto.cs
+++ server/taskflow/DTOs/UserDto.cs
@@ -3,9 +3,10 @@
 public class UserDto
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string ImageUrl { get; set; }
     public Role Role { get; set; } = Role.User;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }

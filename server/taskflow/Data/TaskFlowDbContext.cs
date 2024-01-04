7a8824f5fec9a5adabfd517eb77b744a530e5237
nancytec <adurotimijoshua@gmail.com>
2024-01-04 13:05:04 +0100

    Db context setup

---
+++ server/taskflow/Data/TaskFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace taskflow.Data
+{
+    public class TaskFlowDbContext : DbContext
+    {
+
+        public TaskFlowDbContext(DbContextOptions<TaskFlowDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        // Create properties based on the available entities
+        public DbSet<User> Users { get; set; }
+        
+    }
+}
+
--- server/taskflow/Program.cs
+++ server/taskflow/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using taskflow.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+// Inject TaskFlowDbContext into the app
+builder.Services.AddDbContext<TaskFlowDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("TaskFlowConnectionString"))
+    );
 
 builder.Services.AddEndpointsApiExplorer();
 

cde1876bcab7eb616e269bc1da6ad550da61f4da
Jerry Abadi <abadijerry44@gmail.com>
2024-01-05 21:29:33 +0100

    Frontend:worked on the createOrg page

--- client/taskflow-frontend/app/(landing)/page.tsx
+++ client/taskflow-frontend/app/(landing)/page.tsx
@@ -1,28 +1,26 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import React from 'react';
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
 
 const LandingPage = () => {
   return (
-    <section className='py-2 container flex justify-between'>
+    <section className="py-2 container flex justify-between">
       <div>
-      <Button className="bg-black text-white">Taskflow task manager now public</Button>
-      <br/>
-      Manage your tasks efficiently with ease.
-        
-</div>
-      <div >
+        <Button className="bg-black text-white">
+          Taskflow task manager now public
+        </Button>
+        <br />
+        Manage your tasks efficiently with ease.
+      </div>
+      <div>
         <Image
           src={"/landingIllustration.png"}
-          alt='Illustration'
+          alt="Illustration"
           width={300}
           height={300}
-          className='w-full'
-        
+          className="w-full"
         />
-        </div>
-      
-      
+      </div>
     </section>
   );
 };
--- client/taskflow-frontend/app/layout.tsx
+++ client/taskflow-frontend/app/layout.tsx
@@ -1,21 +1,22 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
-import { siteConfig } from '@/config/site';
+import { siteConfig } from "@/config/site";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
-    template: '%s | ' + siteConfig.name,
+    template: "%s | " + siteConfig.name,
   },
   description: siteConfig.description,
   icons: [
     {
-      url: '/blue-logo.svg',
-      href: '/blue-logo.svg',
+      url: "/blue-logo.svg",
+      href: "/blue-logo.svg",
     },
   ],
 };
@@ -26,8 +27,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body className={inter.className}>
+        {children} <Toaster />
+      </body>
     </html>
   );
 }

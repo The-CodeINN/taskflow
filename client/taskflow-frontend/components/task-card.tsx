d591abf4e3ac886146dc25b11c05208c05ee73df
Jerry Abadi <abadijerry44@gmail.com>
2024-01-10 18:01:07 +0100

    Frontend:worked on the task-card

--- client/taskflow-frontend/components/task-card.tsx
+++ client/taskflow-frontend/components/task-card.tsx
@@ -1,3 +1,4 @@
+import { Pencil, Trash2 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import {
@@ -14,34 +15,39 @@ const TaskCard = () => {
     <>
       <div className="grid gap-4 py-4">
         <div>
-          <h1>Task title</h1>
+          <div className="flex items-center">
+            <h1 className="font-bold">Task title</h1>
+          </div>
+
           <Input type="text" placeholder="Task title" />
           <p className="text-sm text-muted-foreground">in list todo</p>
         </div>
       </div>
       <div className="flex">
         <div className="w-full">
-          <h2>Description</h2>
-          <Textarea placeholder="Task description." className="w-full" />
+          <h2 className="font-bold">Description</h2>
+          <Textarea placeholder="Task description." className="w-[95%]" />
         </div>
-        <div className="px-5 space-y-3">
-          <h1>Actions</h1>
+        <div className="w-[20%] py-2">
+          <h1 className="text-center font-bold">Actions</h1>
           <Button
             type="submit"
-            className="h-[30%] bg-gray-300 hover:bg-gray-400"
+            className="h-[30%] bg-[#191950] w-full font-light  hover:bg-gray-400"
           >
+            <Pencil />
             Update
           </Button>
           <Button
             type="submit"
-            className="h-[30%] bg-gray-300 hover:bg-gray-400"
+            className="h-[30%] bg-[#191950] w-full font-light hover:bg-gray-400"
           >
+            <Trash2 />
             Delete
           </Button>
         </div>
       </div>
       <div>
-        <h1>Assign to</h1>
+        <h1 className="font-bold">Assign to</h1>
         <Select>
           <SelectTrigger className="w-[180px]">
             <SelectValue placeholder="select" />
